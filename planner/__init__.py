937a99b216ea2d508a7d0bc537d5d800840d0700
sandratoo <toosandraa@gmail.com>
2022-05-19 10:02:29 +0300

    first commit

---
+++ planner/__init__.py
@@ -0,0 +1,15 @@
+from ssl import VERIFY_ALLOW_PROXY_CERTS
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SECRET_KEY"] ="secretkey"
+
+    from .views import views
+    app.register_blueprint(views)
+
+    from .auth import auth
+    app.register_blueprint(auth, url_prefix="/auth/")
+
+    return app
+
